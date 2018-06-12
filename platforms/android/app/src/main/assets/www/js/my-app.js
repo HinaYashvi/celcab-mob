d263a2d92a191e40f2b643456ec41884eb283450
unknown <hina.sparmar@gmail.com>
2018-06-12 11:51:39 +0530

    first commit

--- platforms/android/app/src/main/assets/www/js/my-app.js
+++ platforms/android/app/src/main/assets/www/js/my-app.js
@@ -64,9 +64,10 @@ function checkStorage()
       'data':{'city':sess_city,'sess_cust':sess_cust,'sess_mobilenum':sess_mobilenum},
       success:function(response){ 
         if(response){
-          //console.log(response);
+          console.log(response);
           var upcomingride_json_array = $.parseJSON(response);
           var json_upcmride_enrt = upcomingride_json_array.upcomingrides_enroute; 
+          if(json_upcmride_enrt.length > 0){
           for(var i=0;i<json_upcmride_enrt.length;i++){
             var pnr=json_upcmride_enrt[i].id;
             var callbook_cust_id=json_upcmride_enrt[i].customer_master_id;
@@ -75,13 +76,13 @@ function checkStorage()
               $.ajax({ 
                 'type':'POST', 
                 'url':pushnotification_url,
-                'data':{'city':sess_city,'pnr':pnr},
+                'data':{'city':sess_city,'pnr':pnr,'sess_cust':sess_cust},
                 success:function(push_response){ 
                   if(push_response){
                     //console.log(push_response);                 
                     var push_json_array = $.parseJSON(push_response);
                     var json_pushnotify = push_json_array.notifypush;
-                    //console.log(json_pushnotify);
+                    console.log(json_pushnotify.length);
                     for(var j=0;j<json_pushnotify.length;j++){
                       var celcabs_vehicle_id = json_pushnotify[j].celcabs_vehicle_id;
                       var customer_phone1=json_pushnotify[j].phone_no1;
@@ -95,7 +96,7 @@ function checkStorage()
                           'url':driverno_url,
                           'data':{'city':sess_city,'celcabs_vehicle_id':celcabs_vehicle_id},
                           success:function(drvno_response){
-                            //if(drvno_response){
+                            //if(drvno_response){ // cmntd //
                               //console.log("*****"+drvno_response);
                               var push_drv_array = $.parseJSON(drvno_response);
                               var json_drvrno = push_drv_array.driver_ph;
@@ -104,17 +105,19 @@ function checkStorage()
                               for(var k=0;k<json_drvrno.length;k++){
                                 //alert(json_drvrno[k].alt_phone_number);
                                 var driver_phone=json_drvrno[k].alt_phone_number;
-                               // if(driver_phone!=''){
+                               // if(driver_phone!=''){ // cmntd //
                                   //alert(customer_phone1+"---"+customer_phone2);
                                   if(customer_phone1!='' || customer_phone2!=''){
                                     // send push here //
+                                    alert(callbook_cust_id +"=="+ sess_cust);
                                     if(callbook_cust_id == sess_cust){
                                       var push_url = "http://128.199.226.85/mobileapp_celcabs/appcontroller/sendPushMsg";
                                       $.ajax({ 
                                         'type':'POST', 
                                         'url':push_url,
-                                        'data':{'city':sess_city,'celcabs_vehicle_id':celcabs_vehicle_id,'customer_phone1':customer_phone1,'customer_phone2':customer_phone2,'driver_phone':driver_phone,'eatdate':eatdate,'pickuptime':pickuptime},
-                                          success:function(push_response){ 
+                                        'data':{'city':sess_city,'celcabs_vehicle_id':celcabs_vehicle_id,'customer_phone1':customer_phone1,'customer_phone2':customer_phone2,'driver_phone':driver_phone,'eatdate':eatdate,'pickuptime':pickuptime,'sess_cust':sess_cust,'customer_master_id':callbook_cust_id,'sess_mobilenum':sess_mobilenum},
+                                          success:function(push_response){
+                                            alert("push_response");
                                               //console.log(push_response);
                                           }
                                       });
@@ -132,6 +135,8 @@ function checkStorage()
                 }
               });
           }
+
+        }
         } 
       } 
   });
@@ -155,7 +160,7 @@ var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
   };
 
-    window.plugins.OneSignal.startInit("00601283-97fa-455d-ae71-0e064926d8e2").handleNotificationOpened(notificationOpenedCallback).endInit();
+   /* window.plugins.OneSignal.startInit("00601283-97fa-455d-ae71-0e064926d8e2").handleNotificationOpened(notificationOpenedCallback).endInit(); */
 
     if(value==null) 
     {
