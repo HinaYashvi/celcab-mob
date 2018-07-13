05f6c4f72cdcb5f6473215c137c4458dc9b35131
unknown <hina.sparmar@gmail.com>
2018-07-13 16:35:37 +0530

    first commit:

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -44,9 +44,9 @@ var app = new Framework7({
   dynamicNavbar: true,
   pushState: true,
 });*/
-var port = 9013;
+var port = 9013; 
 //var base_url = 'http://128.199.226.85:'+port+'/mobileapp_celcabs/';   // TEST SERVER //
-var base_url = 'http://128.199.226.85:'+port+'/mobileapp_celcabs/';   // LIVE SERVER //
+var base_url = 'http://182.18.160.210:'+port+'/mobileapp_celcabs/';   // LIVE SERVER //
 
 
 $( document ).ready(function() {  
